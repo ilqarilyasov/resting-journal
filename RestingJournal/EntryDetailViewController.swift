d9c68a70fe6069679b420fdd60c87f350d9795fa
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-24 15:51:29 -0600

    Lay out storybard. Create subclasses and add outlet/action connections

---
+++ RestingJournal/EntryDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EntryDetailViewController.swift
+//  RestingJournal
+//
+//  Created by Ilgar Ilyasov on 1/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RestingJournal/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  RestingJournal
+//
+//  Created by Ilgar Ilyasov on 1/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
